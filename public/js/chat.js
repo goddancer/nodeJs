a6d0cb82ece3dca6af97306ec66e0998ee1b6f41
zhangjianchao <kk504712388@qq.com>
2018-04-23 18:16:24 +0800

    添加客户端逻辑

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,7 +1,12 @@
+/*
+* 定义类，接收Socket.IO的参数socket
+* */
 var Chat = function(socket) {
   this.socket = socket;
 };
-
+/*
+* 发送聊天消息
+* */
 Chat.prototype.sendMessage = function(room, text) {
   var message = {
     room: room,
@@ -9,32 +14,34 @@ Chat.prototype.sendMessage = function(room, text) {
   };
   this.socket.emit('message', message);
 };
-
+/*
+* 变更房间
+* */
 Chat.prototype.changeRoom = function(room) {
   this.socket.emit('join', {
     newRoom: room
   });
 };
-
+/*
+* 处理聊天命令
+* */
 Chat.prototype.processCommand = function(command) {
   var words = command.split(' ');
-  var command = words[0]
-                .substring(1, words[0].length)
-                .toLowerCase();
+  var command = words[0].substring(1, words[0].length).toLowerCase();//从第一个单词开始解析命令
   var message = false;
 
   switch(command) {
-    case 'join':
+    case 'join'://处理房间的变换、创建
       words.shift();
       var room = words.join(' ');
       this.changeRoom(room);
       break;
-    case 'nick':
+    case 'nick'://处理更名尝试
       words.shift();
       var name = words.join(' ');
       this.socket.emit('nameAttempt', name);
       break;
-    default:
+    default://如果命令无法识别，返回错误信息
       message = 'Unrecognized command.';
       break;
   };
--- public/js/chat_ui.js
+++ public/js/chat_ui.js
@@ -1,3 +1,6 @@
+/*
+* 将特殊字符转换为HTML实体，防止跨域脚本攻击。
+* */
 function divEscapedContentElement(message) {
   return $('<div></div>').text(message);
 }
@@ -5,17 +8,19 @@ function divEscapedContentElement(message) {
 function divSystemContentElement(message) {
   return $('<div></div>').html('<i>' + message + '</i>');
 }
-
+/*
+* 处理原始的用户输入
+* */
 function processUserInput(chatApp, socket) {
   var message = $('#send-message').val();
   var systemMessage;
 
-  if (message.charAt(0) == '/') {
+  if (message.charAt(0) == '/') {//如果用户输入的内容以（/）开头，将其作为聊天命令
     systemMessage = chatApp.processCommand(message);
     if (systemMessage) {
       $('#messages').append(divSystemContentElement(systemMessage));
     }
-  } else {
+  } else {//将非命令输入广播给其他用户
     chatApp.sendMessage($('#room').text(), message);
     $('#messages').append(divEscapedContentElement(message));
     $('#messages').scrollTop($('#messages').prop('scrollHeight'));
@@ -23,13 +28,15 @@ function processUserInput(chatApp, socket) {
 
   $('#send-message').val('');
 }
-
+/*
+* 客户端程序初始化
+* */
 var socket = io.connect();
 
 $(document).ready(function() {
   var chatApp = new Chat(socket);
 
-  socket.on('nameResult', function(result) {
+  socket.on('nameResult', function(result) {//显示更名尝试的结果
     var message;
 
     if (result.success) {
@@ -40,17 +47,17 @@ $(document).ready(function() {
     $('#messages').append(divSystemContentElement(message));
   });
 
-  socket.on('joinResult', function(result) {
+  socket.on('joinResult', function(result) {//显示房间变更的结果
     $('#room').text(result.room);
     $('#messages').append(divSystemContentElement('Room changed.'));
   });
 
-  socket.on('message', function (message) {
+  socket.on('message', function (message) {//显示接收的消息
     var newElement = $('<div></div>').text(message.text);
     $('#messages').append(newElement);
   });
 
-  socket.on('rooms', function(rooms) {
+  socket.on('rooms', function(rooms) {//显示可用房间列表
     $('#room-list').empty();
 
     for(var room in rooms) {
@@ -60,19 +67,19 @@ $(document).ready(function() {
       }
     }
 
-    $('#room-list div').click(function() {
+    $('#room-list div').click(function() {//点击房间名可以跳转
       chatApp.processCommand('/join ' + $(this).text());
       $('#send-message').focus();
     });
   });
 
-  setInterval(function() {
+  setInterval(function() {//定期请求可用房间列表
     socket.emit('rooms');
   }, 1000);
 
   $('#send-message').focus();
 
-  $('#send-form').submit(function() {
+  $('#send-form').submit(function() {//提交表单可以发送聊天消息
     processUserInput(chatApp, socket);
     return false;
   });
